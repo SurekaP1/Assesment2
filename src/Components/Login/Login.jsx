ba8a589032c8d97ec654ebc5b7718767d4a32223
SurekaP1 <psureka@std.foc.sab.ac.lk>
2025-03-23 00:18:03 -0700

    updated all files and added untracked files

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Login.css'
-import SideImage from '../../assets/SideImage.png'
+
 import Footer from '../Footer/Footer'
 import TopHeader from '../TopHeader/TopHeader'
 import Header from '../Header/Header'
 
 const Login = () => {
   return (
-    <div className="login-import">
+    <div >
       <TopHeader/>
       <Header/>
      
